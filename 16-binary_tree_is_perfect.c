23e1afd2f867a7ef14a08619055d8cfb9f5a9713
OscarM03 <oscarmutwiri03@gmail.com>
2024-01-31 18:21:18 +0300

    Checks if the tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -46,19 +46,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 }
 
 /**
- * max - Finds the maximum of two integers.
- * @a: The first integer.
- * @b: The second integer.
- * Return: the larger int
- * The larger of the two integers:
- * @a if @a > @b, or @b otherwise.
- */
-int max(int a, int b)
-{
-	return ((a > b) ? a : b);
-}
-
-/**
  * binary_tree_height - Measures the height of a binary tree.
  * @tree: Pointer to the root node of the tree to measure.
  *
