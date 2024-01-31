db8aabce26b5fa2344ffd04ace14af7bd3d629cc
OscarM03 <oscarmutwiri03@gmail.com>
2024-01-31 14:02:35 +0300

    Solved the functionality of height function to handle other cases

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -27,6 +27,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int left_height = 0, right_height = 0, height;
 
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	
     if (tree->left != NULL)
     {
         left_height = 1 + binary_tree_height(tree->left);
